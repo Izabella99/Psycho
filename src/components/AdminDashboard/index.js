4c1a99643df470de268c25aea5ead13dc7192fef
Flaviu <flaviu.marasescu@nagarro.com>
2022-05-08 21:44:25 +0300

    Added header and nav bar

--- src/components/AdminDashboard/index.js
+++ src/components/AdminDashboard/index.js
@@ -5,11 +5,11 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Button } from '@mui/material';
 import { useState } from 'react';
-import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
+import '../../assets/css/Chat.css'
+import Nav from '../chat/Nav';
+import Header from '../Header';
 
 import './index.css';
 
@@ -50,110 +50,119 @@ const Dashboard = () => {
   const [select, setSelect] = useState(1);
 
   return (
-    <div className='admin'>
-      <div className='admin-dashboard-page'>
-        <h1 className='admin-dashboard-title'>Dashboard</h1>
-        <div className='admin-dashboard-students'>
-          <div className='admin-dashboard-students-col'>
-            <div>Students with coordinator</div>
-            <div className='number'>10</div>
+    <div className='f-layer'>
+      <Nav />
+
+      <div className='f-container'>
+        <div className='f-header'>
+            <Header />
           </div>
-          <div className='admin-dashboard-students-col'>
-            <div>Students without coordinator</div>
-            <div className='number'>22</div>
+        <div className='admin'>
+          <div className='admin-dashboard-page'>
+            <h1 className='admin-dashboard-title'>Dashboard</h1>
+            <div className='admin-dashboard-students'>
+              <div className='admin-dashboard-students-col'>
+                <div>Students with coordinator</div>
+                <div className='number'>10</div>
+              </div>
+              <div className='admin-dashboard-students-col'>
+                <div>Students without coordinator</div>
+                <div className='number'>22</div>
+              </div>
+              <div className='admin-dashboard-students-col'>
+                <div>Internship students</div>
+                <div className='number'>53</div>
+              </div>
+              <div className='admin-dashboard-students-col'>
+                <div>Available places</div>
+                <div className='number'>20</div>
+              </div>
+            </div>
           </div>
-          <div className='admin-dashboard-students-col'>
-            <div>Internship students</div>
-            <div className='number'>53</div>
+        
+          <div className='wrapper'>
+            <Box sx={{ width: '100%' }}>
+              <Grid
+                container
+                rowSpacing={0}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              >
+                <Grid className='grid' item xs={6}>
+                  <Item className='item-col'>
+                    <div>Students state</div>
+                    <Pie
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: false,
+                      }}
+                      data={data}
+                    />
+                  </Item>
+                </Grid>
+                <Grid className='grid' item xs={6}>
+                  <Item className='item'>
+                    <div className='item-requests-col'>
+                      <div className=''>
+                        <div>Requests sent</div>
+                        <div className='number'>10</div>
+                      </div>
+                      <div>
+                        <div>Pending request</div>
+                        <div className='number'>22</div>
+                      </div>
+                    </div>
+                    <div className='item-requests-col'>
+                      <div>
+                        <div>Accepted</div>
+                        <div className='number'>53</div>
+                      </div>
+                      <div>
+                        <div>Rejected</div>
+                        <div className='number'>20</div>
+                      </div>
+                    </div>
+                  </Item>
+                </Grid>
+                <Grid className='grid' item xs={6}>
+                  <Item className='item-col'>
+                    <div>Students state</div>
+                    <Pie
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: false,
+                      }}
+                      data={data}
+                    />
+                  </Item>
+                </Grid>
+                <Grid className='grid' item xs={6}>
+                  <Item className='item-col'>
+                    <div>Students state</div>
+                    <Pie
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: false,
+                      }}
+                      data={data}
+                    />
+                  </Item>
+                </Grid>
+              </Grid>
+            </Box>
           </div>
-          <div className='admin-dashboard-students-col'>
-            <div>Available places</div>
-            <div className='number'>20</div>
+        
+          <div className='export'>
+            <span className='export-label'>Select document to export</span>
+            <TextField
+            className="export-select"
+              id="outlined-select-currency"
+              select
+              label="File"
+              value={select}
+            ></TextField>
+            <button className='export-btn' variant='contained'>Export</button>
           </div>
         </div>
-      </div>
-
-      <div className='wrapper'>
-        <Box sx={{ width: '100%' }}>
-          <Grid
-            container
-            rowSpacing={0}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            <Grid className='grid' item xs={6}>
-              <Item className='item-col'>
-                <div>Students state</div>
-                <Pie
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: false,
-                  }}
-                  data={data}
-                />
-              </Item>
-            </Grid>
-            <Grid className='grid' item xs={6}>
-              <Item className='item'>
-                <div className='item-requests-col'>
-                  <div className=''>
-                    <div>Requests sent</div>
-                    <div className='number'>10</div>
-                  </div>
-                  <div>
-                    <div>Pending request</div>
-                    <div className='number'>22</div>
-                  </div>
-                </div>
-                <div className='item-requests-col'>
-                  <div>
-                    <div>Accepted</div>
-                    <div className='number'>53</div>
-                  </div>
-                  <div>
-                    <div>Rejected</div>
-                    <div className='number'>20</div>
-                  </div>
-                </div>
-              </Item>
-            </Grid>
-            <Grid className='grid' item xs={6}>
-              <Item className='item-col'>
-                <div>Students state</div>
-                <Pie
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: false,
-                  }}
-                  data={data}
-                />
-              </Item>
-            </Grid>
-            <Grid className='grid' item xs={6}>
-              <Item className='item-col'>
-                <div>Students state</div>
-                <Pie
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: false,
-                  }}
-                  data={data}
-                />
-              </Item>
-            </Grid>
-          </Grid>
-        </Box>
-      </div>
-
-      <div className='export'>
-        <span className='export-label'>Select document to export</span>
-        <TextField
-        className="export-select"
-          id="outlined-select-currency"
-          select
-          label="File"
-          value={select}
-        ></TextField>
-        <button className='export-btn' variant='contained'>Export</button>
       </div>
     </div>
   );
