cecef7c9ef63b5f59e0e2782911d9c34ef5b63db
Flaviu <flaviu.marasescu@nagarro.com>
2022-05-22 21:11:32 +0300

    Auth functionality,students request, teacher accept/reject requests

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { useNavigate } from "react-router-dom"
-import "../assets/css/Login.css";
+import { useNavigate } from 'react-router-dom';
+import '../assets/css/Login.css';
 import '@fontsource/roboto/300.css';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -11,80 +11,142 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Header from './Header';
 
-
-
 const Login = () => {
   window.scrollTo(0, 0);
   const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      const email = data.get('email')
-      const password = data.get('password')
-      let role;
-      if(email.includes('student')){
-        role = 'student';
-      } else if(email.includes('teacher')){
-        role = 'teacher'
-      }
-      localStorage.setItem('email', email)
-      localStorage.setItem('password', password)
-      localStorage.setItem('role', role)
-      navigate('/dashboard')
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+
+    let role;
+    if (email.includes('student')) {
+      const user = await fetch(`http://localhost:3001/api/student`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }).then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.text();
+      });
+      console.log('user', user[0].name);
+      role = 'student';
+      localStorage.setItem('name', user[0].name);
+      localStorage.setItem('email', email);
+      localStorage.setItem('password', password);
+      localStorage.setItem('role', role);
+      navigate('/professors');
+    } else if (email.includes('professor')) {
+      const professor = await fetch(`http://localhost:3001/api/professor`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }).then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.text();
+      });
+      console.log('user', professor);
+
+      role = 'professor';
+      localStorage.setItem('name', professor[0].name);
+      localStorage.setItem('email', email);
+      localStorage.setItem('password', password);
+      localStorage.setItem('role', role);
+      navigate('/requests');
+    } else if (email.includes('admin')) {
+      const admin = await fetch(`http://localhost:3001/api/admin`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }).then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.text();
+      });
+      role = 'admin';
+      localStorage.setItem('name', admin[0].name);
+      localStorage.setItem('email', email);
+      localStorage.setItem('password', password);
+      localStorage.setItem('role', role);
+      navigate('/dashboard');
+    }
+  };
 
   return (
-    <div className="login-page">
-            <Container 
-            component="main" 
-            maxWidth="xs" 
-            sx={{
-                borderRadius: '10px',
-                backgroundColor: 'rgba(229, 229, 229, 100)' ,
-                border:' 1px solid rgba(187, 187, 187, 100)',
-                marginTop: '100px',
-                }}
+    <div className='login-page'>
+      <Container
+        component='main'
+        maxWidth='xs'
+        sx={{
+          borderRadius: '10px',
+          backgroundColor: 'rgba(229, 229, 229, 100)',
+          border: ' 1px solid rgba(187, 187, 187, 100)',
+          marginTop: '100px',
+        }}
+      >
+        <CssBaseline />
+        <Box className='login-box'>
+          <Avatar></Avatar>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
             >
-                <CssBaseline />
-                <Box className="login-box">
-                <Avatar></Avatar>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    />
-                    <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    />
-                    <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Sign In
-                    </Button>
-                    <Link href="#" variant="body2">
-                        Forgot password?
-                    </Link>  
-                </Box>
-                </Box>
-            </Container> 
-      </div>
+              Sign In
+            </Button>
+            <Link href='#' variant='body2'>
+              Forgot password?
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </div>
   );
 };
 export default Login;
--- src/components/chat/Nav.jsx
+++ src/components/chat/Nav.jsx
@@ -1,36 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import "../../assets/css/Nav.css";
-import { BsFillPeopleFill, BsFillPersonFill, BsFillChatFill, BsFillPersonBadgeFill } from 'react-icons/bs';
+import '../../assets/css/Nav.css';
+import {
+  BsFillPeopleFill,
+  BsFillPersonFill,
+  BsFillChatFill,
+  BsFillPersonBadgeFill,
+} from 'react-icons/bs';
 import { FaGraduationCap } from 'react-icons/fa';
-import { DiRequirejs } from "react-icons/di";
+import { DiRequirejs } from 'react-icons/di';
 
 function Nav() {
-  return (
-    <div className='nav'>
-            <Link to='/professors' className='nav-item'>
-                <BsFillPersonBadgeFill /> Professors
-            </Link>
-            <Link to='/students' className='nav-item'>
-                <BsFillPeopleFill /> Students
-            </Link>
-            <Link to='/profile' className='nav-item'>
-                <BsFillPersonFill /> Profile
-            </Link>
-            <Link to='/requests' className='nav-item'>
-                <DiRequirejs/> Requests
-            </Link>
-            <Link to='/grades' className='nav-item'>
-                <FaGraduationCap /> Grades
-            </Link>
-            <Link to='/chat' className='nav-item'>
-                <BsFillChatFill /> Chat
-            </Link>
-            <div className='nav-footer'>
-                <BsFillPersonFill /> Name Name
-            </div>
-    </div>
-  );
-};
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
+
+  useEffect(() => {
+    const email = localStorage.getItem('email');
+    const role = localStorage.getItem('role');
+    const name = localStorage.getItem('name');
+    setName(name);
+    setRole(role);
+    console.log(email);
+    console.log(role);
+  }, []);
+
+  if (role === 'student') {
+    return (
+      <div className='nav'>
+        <Link to='/professors' className='nav-item'>
+          <BsFillPersonBadgeFill /> Professors
+        </Link>
+        <Link to='/profile' className='nav-item'>
+          <BsFillPersonFill /> Profile
+        </Link>
+        <Link to='/grades' className='nav-item'>
+          <FaGraduationCap /> Grades
+        </Link>
+        <div className='nav-footer'>
+          <BsFillPersonFill /> {name}
+        </div>
+      </div>
+    );
+  } else if (role === 'professor') {
+    return (
+      <div className='nav'>
+        <Link to='/students' className='nav-item'>
+          <BsFillPeopleFill /> Students
+        </Link>
+        <Link to='/profile' className='nav-item'>
+          <BsFillPersonFill /> Profile
+        </Link>
+        <Link to='/requests' className='nav-item'>
+          <DiRequirejs /> Requests
+        </Link>
+        <div className='nav-footer'>
+          <BsFillPersonFill /> {name}
+        </div>
+      </div>
+    );
+  } else if(role === 'admin') {
+    return (
+      <div className='nav'>
+        <Link to='/professors' className='nav-item'>
+          <BsFillPersonBadgeFill /> Professors
+        </Link>
+        <Link to='/students' className='nav-item'>
+          <BsFillPeopleFill /> Students
+        </Link>
+        <Link to='/profile' className='nav-item'>
+          <BsFillPersonFill /> Profile
+        </Link>
+        <Link to='/requests' className='nav-item'>
+          <DiRequirejs /> Requests
+        </Link>
+        <Link to='/grades' className='nav-item'>
+          <FaGraduationCap /> Grades
+        </Link>
+        <Link to='/chat' className='nav-item'>
+          <BsFillChatFill /> Chat
+        </Link>
+        <div className='nav-footer'>
+          <BsFillPersonFill /> {name}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Nav;
